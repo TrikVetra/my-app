5511ec9f645fe62bfdca1f58ff9bd154395a1f0f
Tanya <tastr@outlook.com>
2022-01-11 06:35:24 +0300

    added Data massives

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -17,23 +17,39 @@ const Message = (props) => {
     )
 }
 
+
+
 const Dialogs = (props) => {
+
+     let dialogsData = [
+         {id: 1, name: 'Vika'},
+         {id: 2, name: 'Tanya'},
+         {id: 3, name: 'Mika'}
+    ];
+
+    let messagesData = [
+        { id: 1, message: 'Привет' },
+        { id: 2, message: 'Как дела?' },
+        { id: 3, message: 'Хорошо' },
+        { id: 3, message: 'Как всегда' }
+    ]
+
     return (
-        <div className={classes.Dialogs}>
+        <div className = {classes.Dialogs}>
 
-            <div className={classes.Dialog_items}>
+            <div className = {classes.Dialog_items}>
 
-                <Dialog_item id='1' name='Vika'/>
-                <Dialog_item id='2' name='Tanya'/>
-                <Dialog_item id='3' name='Mika'/>
+                <Dialog_item id = {dialogsData[0].id} name = {dialogsData[0].name}/>
+                <Dialog_item id = {dialogsData[1].id} name = {dialogsData[1].name}/>
+                <Dialog_item id = {dialogsData[2].id} name = {dialogsData[2].name}/>
 
             </div>
 
             <div className={classes.Messages}>
-                <Message message='Привет'/>
-                <Message message='Как дела?'/>
-                <Message message='Хорошо'/>
-                <Message message='Как всегда'/>
+                <Message id = {messagesData[0].id} message = {messagesData[0].message}/>
+                <Message id = {messagesData[1].id} message = {messagesData[1].message}/>
+                <Message id = {messagesData[2].id} message = {messagesData[2].message}/>
+                <Message id = {messagesData[3].id} message = {messagesData[3].message}/>
 
             </div>
         </div>
