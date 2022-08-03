60cbd0082163734db51661543bd5c8d1d36e29cd
Tanya S <johndoe@example.com>
2022-08-03 09:42:20 +0300

    97 16:00

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,7 @@
-//import Autor from "./Autor/Autor";
-import classes from './Autor/Autor.module.css';
+
+import classes from './ProfileInfo.module.css';
 import userPhoto from '../../../assets/img/monkey.png';
-import Preloader from '../../Common/Preloader/Preloader'
+import Preloader from '../../Common/Preloader/Preloader';
 import ProfileStatusWithHooks from './ProfileStatus/ProfileStatusWithHooks';
 
 const ProfileInfo = (props) => {
@@ -24,27 +24,48 @@ const ProfileInfo = (props) => {
 
                 <img alt='hear' src={props.profile.photos.large || userPhoto}></img>
                 {props.profile.userId === props.auth.id ? <input type={"file"} onChange={photoSelected}/> : null }
+                <ProfileData profile = {props.profile}/>
+                
+            </div>
+            
+        </div>
+    )
 
-                <div className={classes.autor_description}>
-                    <div> 
-                        <b>Имя:</b> {props.profile.fullName}
-                    </div>
-                    <div>
-                        <b>О себе:</b> {props.profile.aboutMe}<br></br>
-                    </div>
-                    <div>
-                        <b>Ищещь работу?</b> {props.profile.fullName.lookingForAJob ? "Да" : "Нет"}<br></br>
-                    </div>
-                        {props.profile.fullName.lookingForAJob 
-                            ? <div><b>Что умеешь?</b> {props.profile.lookingForAJobDescription}</div>  
-                            : null
-                        }
-                    
-                </div>
+}
+
+const ProfileData = ({profile}) => {
+    return (
+        <div className={classes.autor_description}>
+            <div>
+                <b>Имя:</b> {profile.fullName}
+            </div>
+            <div>
+                <b>О себе:</b> {profile.aboutMe}<br></br>
+            </div>
+            <div>
+                <b>Ищещь работу?</b> {profile.fullName.lookingForAJob ? "Да" : "Нет"}<br></br>
+            </div>
+            {profile.fullName.lookingForAJob
+                ? <div><b>Что умеешь?</b> {profile.lookingForAJobDescription}</div>
+                : null
+            }
+            <div> <b>Contacts:</b><br></br>
+                {Object.keys(profile.contacts).map(key => {
+                    if (profile.contacts[key]) {
+                        return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]} />
+                    }
+                })}
             </div>
         </div>
-    );
+    )
+}
 
+const Contact = ({ contactTitle, contactValue }) => {
+    return (
+        <div className={classes.contacts}>
+            <small><b>{contactTitle}</b>: {contactValue}</small>
+        </div>
+    )
 }
 
 export default ProfileInfo
