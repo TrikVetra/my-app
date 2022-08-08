fa9d9e407975f3d9ed783c0ffe877c539e2d47eb
Tanya S <johndoe@example.com>
2022-08-08 17:45:30 +0300

    97 22:00

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,8 +3,12 @@ import classes from './ProfileInfo.module.css';
 import userPhoto from '../../../assets/img/monkey.png';
 import Preloader from '../../Common/Preloader/Preloader';
 import ProfileStatusWithHooks from './ProfileStatus/ProfileStatusWithHooks';
+import React, { useState } from "react"
 
 const ProfileInfo = (props) => {
+
+    let [editMode, setEditMode] = useState(false)
+    let isOwner = false    
 
     const photoSelected = (e) => {
         if (e.target.files.length) {
@@ -13,29 +17,64 @@ const ProfileInfo = (props) => {
     }
 
     if (!props.profile) { //Если данных ещё нет, грузим прелоадер.
-        return (<Preloader/>)
-    } else  return (  
+        return (<Preloader />)
+    } else return (
         <div className={classes.profile_wrapper}>
+
             <div className={classes.status}>
                 <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus} />
-                
             </div>
             <div className={classes.autor}>
 
                 <img alt='hear' src={props.profile.photos.large || userPhoto}></img>
-                {props.profile.userId === props.auth.id ? <input type={"file"} onChange={photoSelected}/> : null }
-                <ProfileData profile = {props.profile}/>
+                {props.profile.userId === props.auth.id ? isOwner = true : isOwner = false}
+                <div>{isOwner ? <input type={"file"} onChange={photoSelected} /> : null}</div>
                 
+                {editMode 
+                ? <ProfileDataForm profile={props.profile} toggleEditMode={ () => {setEditMode(false)} }/> 
+                : <ProfileData profile={props.profile} toggleEditMode={ () => {setEditMode(true)} } isOwner={isOwner} />
+                }
+                {editMode}
+
             </div>
-            
+
         </div>
     )
 
 }
 
-const ProfileData = ({profile}) => {
+const ProfileData = ({profile, toggleEditMode, isOwner}) => {
     return (
         <div className={classes.autor_description}>
+             {isOwner ? <button onClick={toggleEditMode}>Редактировать профиль</button> : null}
+            <div>
+                <b>Имя:</b> {profile.fullName}
+            </div>
+            <div>
+                <b>О себе:</b> {profile.aboutMe}<br></br>
+            </div>
+            <div>
+                <b>Ищещь работу?</b> {profile.fullName.lookingForAJob ? "Да" : "Нет"}<br></br>
+            </div>
+            {profile.fullName.lookingForAJob
+                ? <div><b>Что умеешь?</b> {profile.lookingForAJobDescription}</div>
+                : null
+            }
+            <div> <b>Contacts:</b><br></br>
+                {Object.keys(profile.contacts).map(key => {
+                    if (profile.contacts[key]) {
+                        return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]} />
+                    }
+                })}
+            </div>
+        </div>
+    )
+}
+
+const ProfileDataForm = ({profile, toggleEditMode}) => {
+    return (
+        <div className={classes.autor_description}>            
+            <button onClick={toggleEditMode}>Сохранить изменения</button>
             <div>
                 <b>Имя:</b> {profile.fullName}
             </div>
